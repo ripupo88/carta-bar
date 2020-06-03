c2f05c60aaa0b8854dc0e2d94bd7aca4bb597338
Richar Pupo <44076974+ripupo88@users.noreply.github.com>
2020-06-03 05:02:07 +0100

    firts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div class="container">
+        <h1> Normalish Food </h1>
+        <ul class="leaders">
+          <li>
+            <span>Grilled Cheese</span>
+            <span>7.95</span>
+          </li>
+          <li>
+            <span>Wonton Soup</span>
+            <span>8.95</span>
+          </li>
+          <li>
+            <span>Waffles</span>
+            <span>7.95</span>
+          </li>
+          <li>
+            <span>Shrimp Etouffee</span>
+            <span>15.25</span>
+          </li>
+          <li>
+            <span>Miso Ramen</span>
+            <span>10.95</span>
+          </li>
+        </ul>
+        <hr class="fancy-rule asterism"/>
+        <hr class="fancy-rule asterism rgba"/>
+        <h1> Delicacies </h1>
+        <ul class="leaders">
+          <li>
+            <span>Bear Foot Soup</span>
+            <span>7.95</span>
+          </li>
+          <li>
+            <span>Fried Raptor Eggs</span>
+            <span>24.95</span>
+          </li>
+          <li>
+            <span>Raspberry Asparagus Creme Brulée</span>
+            <span>13.95</span>
+          </li>
+        </ul>
+        </div>
+        </div>
   );
 }
 
